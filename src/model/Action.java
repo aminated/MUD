6b71598126edd744f5b84f1596509f6a0fd6b1f3
Andrey Kuklev <andreyk@email.arizona.edu>
2013-04-11 13:45:01 -0700

    Fix classpath issue.

---
+++ src/model/Action.java
@@ -0,0 +1,15 @@
+package model;
+/**
+ * An action to be executed by a living entity. 
+ * @author Andrey Kuklev
+ *
+ */
+public class Action {
+	
+	/**
+	 * Parse a command and create an action. 
+	 */
+	public Action(String command){
+		
+	}
+}
