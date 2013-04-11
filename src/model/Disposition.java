3e19e7098cb41c483e4bae3a18df8c06816b2df6
Andrey Kuklev <andreyk@email.arizona.edu>
2013-04-11 14:48:23 -0700

    Add basic implementation for Disposition. Begin PlayerDisposition.

--- src/model/Disposition.java
+++ src/model/Disposition.java
@@ -1,13 +1,20 @@
 package model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
- * A strategy for Mobs
+ * A strategy for Mobs and players.
+ * This is where all commands come from. This class is responsible for coming up with new Actions and adding them
+ * to its private queue, to be executed on the next tick. How it comes up with the Actions is up to the specific subclass.
+ * We may want subclasses of this to be separate threads so they can run concurrently with the timer. 
  */
 public abstract class Disposition {
-	private List<Action> queue;
-	private Living owner;
+	protected List<Action> queue = new LinkedList<Action>();
+	protected Living owner;
+	public boolean hasNext(){
+		return !queue.isEmpty();
+	}
 	public void doNext(){
 		if(!queue.isEmpty()){
 			Action action = queue.get(0);
---
+++ src/model/PlayerDisposition.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+public class PlayerDisposition extends Disposition {
+	private ClientListener listener;
+	private class ClientListener extends Thread{
+		public ObjectInputStream stream;
+		public void run(){
+			while(true){
+				String command = "";
+				try {
+					command = (String) stream.readObject();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Handle dropped connection.
+					e.printStackTrace();
+				}
+				queue.add(new Action(owner, command));
+			}
+		}
+	}
+	public PlayerDisposition(Socket client){
+		this.listener = new ClientListener();
+		listener.stream = new ObjectInputStream( client.getInputStream() );
+		
+	}
+}
