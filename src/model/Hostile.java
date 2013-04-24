745916ebe65c8e039eb5a61ff9edc4813e5d18cf
Xiaoming Zhou <zhouxm26@gmail.com>
2013-04-23 23:56:26 -0700

    Code Hostile disposition

--- src/model/Hostile.java
+++ src/model/Hostile.java
@@ -1,5 +1,7 @@
 package model;
 
+import items.Item;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -26,13 +28,36 @@ public class Hostile extends Disposition {
 			queue.remove(0);
 			location.announce(action);
 		}
+		else{
+			attack();	// If action list is empty, attack something in the room.
+		}
 	}
+	/**
+	 * Look for Mobs or player in the same room to attack.
+	 * 
+	 */
+	public void attack(){
+		Targetable prey=owner.getRoom().seek();
+		if (prey==null || prey instanceof Item)
+			leave();	// If the room is empty (A room without door, weird) or prey is not living thing, Mob leaves this room.
+		else{
+			addAction( new Action(owner, prey));
+		}
+	}
+	public void leave(){
+		Targetable target=owner.getRoom().seek();
+		if (target instanceof Door)
+			addAction( new Action(owner, target));	// If Mob find a door, it can leave. Otherwise, it stays in the room
+	}
+	
 	/**
 	 * Alert this Disposition of an event that happened in the room. 
 	 * @param event An Action just executed by another creature in the room. 
 	 */
 	public void notify(Action event){
-		
+		if(event.getSource() instanceof Player && event.getTarget() instanceof Door) // Otherwise Mobs will fight each other
+			  if( event.getSource().getRoom().equals(owner.getRoom() ) )// Make sure it's not them leaving the room.
+				  addAction( new Action(owner, event.getSource()));  // Attack whoever entered the room
 	}
 	
 	/**
@@ -43,3 +68,4 @@ public class Hostile extends Disposition {
 		
 	}
 }
+
